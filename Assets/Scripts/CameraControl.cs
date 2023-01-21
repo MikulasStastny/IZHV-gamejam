f97b31f45605e26857536326bc81b40f1603242c
Mikuláš Šťastný <xstast38@stud.fit.vutbr.cz>
2023-01-21 10:48:18 +0100

    camera behaviour fixed

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -8,23 +8,36 @@ public class CameraControl : MonoBehaviour
     //public Vector3 offset = new Vector3(0, 4, -10);
     public float smoothTime = 0.125f;
     Vector3 currentVelocity;
+    public Camera mainCamera;
+
+    private float screenHalfWidth;
+    private float xLeftStopper;
+    private float xRightStopper;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        // Calculates left and right stoppers in world space based on screen width.
+        // Camera won't follow player after these stoppers.
+        screenHalfWidth = Mathf.Abs(mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, -10)).x
+                                    - mainCamera.ViewportToWorldPoint(new Vector3(0, 0, -10)).x);
+        xLeftStopper = -10f + screenHalfWidth;
+        xRightStopper = 40f - screenHalfWidth;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        Vector3 cameraPosition = new Vector3(target.position.x, 0, -10);
+    void FixedUpdate(){
+        Vector3 cameraPosition = new Vector3(0, 0, -10);
 
-        if(cameraPosition.x < 0.25){
-            cameraPosition.x = 0.25f;
+        if(target.position.x < xLeftStopper){
+            cameraPosition.x = xLeftStopper;
         }
-        else if(cameraPosition.x > 29.75){
-            cameraPosition.x = 29.75f;
+        else if(target.position.x > xRightStopper){
+            cameraPosition.x = xRightStopper;
         }
+        else{
+            cameraPosition.x = target.position.x;
+        }
+
         transform.position = Vector3.SmoothDamp(transform.position, cameraPosition, ref currentVelocity, smoothTime);
     }
 }
