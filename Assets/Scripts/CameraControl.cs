5a4f61cc2a901e2b63dbc549a1dfb4f7c258f61c
Mikuláš Šťastný <xstast38@stud.fit.vutbr.cz>
2023-01-21 12:24:11 +0100

    Added vertical camera movement

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -9,10 +9,13 @@ public class CameraControl : MonoBehaviour
     public float smoothTime = 0.125f;
     Vector3 currentVelocity;
     public Camera mainCamera;
-
+    public float platformHeight = 4f;
     private float screenHalfWidth;
-    private float xLeftStopper;
-    private float xRightStopper;
+    private float screenHalfHeight;
+    private float leftStopper;
+    private float rightStopper;
+    private float downStopper;
+    private float upStopper;
 
     // Start is called before the first frame update
     void Start(){
@@ -20,22 +23,37 @@ public class CameraControl : MonoBehaviour
         // Camera won't follow player after these stoppers.
         screenHalfWidth = Mathf.Abs(mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, -10)).x
                                     - mainCamera.ViewportToWorldPoint(new Vector3(0, 0, -10)).x);
-        xLeftStopper = -10f + screenHalfWidth;
-        xRightStopper = 40f - screenHalfWidth;
+        screenHalfHeight = Mathf.Abs(mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, -10)).y
+                                    - mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0, -10)).y);
+        leftStopper = -10f + screenHalfWidth;
+        rightStopper = 40f - screenHalfWidth;
+        downStopper = -1f + screenHalfHeight;
+        upStopper = 30f - screenHalfHeight;
     }
 
     // Update is called once per frame
     void FixedUpdate(){
-        Vector3 cameraPosition = new Vector3(0, 0, -10);
+        Vector3 cameraPosition = new Vector3(0, 1, -10);
 
-        if(target.position.x < xLeftStopper){
-            cameraPosition.x = xLeftStopper;
+        if(target.position.x < leftStopper){
+            cameraPosition.x = leftStopper;
         }
-        else if(target.position.x > xRightStopper){
-            cameraPosition.x = xRightStopper;
+        else if(target.position.x > rightStopper){
+            cameraPosition.x = rightStopper;
         }
         else{
             cameraPosition.x = target.position.x;
+        }
+
+        if(target.position.y < downStopper){
+            cameraPosition.y = downStopper;
+        }
+        else if(target.position.y > upStopper){
+            cameraPosition.y = upStopper;
+        }
+        else{
+            int platform = (int)(target.position.y/platformHeight);
+            cameraPosition.y += platform*platformHeight;
         }
 
         transform.position = Vector3.SmoothDamp(transform.position, cameraPosition, ref currentVelocity, smoothTime);
