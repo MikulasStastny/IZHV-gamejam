3821c8963a2ab10db77a83090f6b78d84f449bb5
Mikuláš Šťastný <xstast38@stud.fit.vutbr.cz>
2023-01-23 00:47:14 +0100

    [WIP] MainMenu buttons works

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
 {
+    //public Button level1, level2;
     public void LoadScene(string sceneName){
+        print("in load");
         SceneManager.LoadScene(sceneName);
     }
+
+    /*void Start() {
+        level1.onClick.AddListener(delegate {LoadScene("Level_01");});
+        level2.onClick.AddListener(delegate {LoadScene("Level_02");});
+    }*/
 }
