ec7218bbcb731530cc29bbc5b9192da5b1c327bc
Daniel Kříž <daniel.kriz@protonmail.com>
2023-01-23 01:32:49 +0100

    fix main menu buttons

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -6,14 +6,8 @@ using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
 {
-    //public Button level1, level2;
     public void LoadScene(string sceneName){
-        print("in load");
+        print("in load" + sceneName);
         SceneManager.LoadScene(sceneName);
     }
-
-    /*void Start() {
-        level1.onClick.AddListener(delegate {LoadScene("Level_01");});
-        level2.onClick.AddListener(delegate {LoadScene("Level_02");});
-    }*/
 }
